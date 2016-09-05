8f5a0b6764022279b3910b2a99fdae19eb4bddea
BrusherYT <kaanmob@gmail.com>
2016-09-05 09:17:56 +0200

    Add project files.

---
+++ 00_Hello/Functions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _00_Hello
+{
+    internal class Functions
+    {
+        public static string Hello()
+        {
+            return "Hello";
+        }
+
+        public static string Hello(string name)
+        {
+            return "hello, " + name;
+        }
+    }
+}
